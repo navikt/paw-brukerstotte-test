915d60844d23716722f371918b8a6f68ea2f21ec
Christian Rennemo <christian.rennemo@nav.no>
2026-01-29 10:36:29 +0100

    legg til funksjonalitet for kopier-knappen

--- static/app.js
+++ static/app.js
@@ -1,8 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
+const handleCopyButtonClick = (button) => {
+  const textToCopy = button.getAttribute("data-text-copy");
+  navigator.clipboard.writeText(textToCopy);
+
+  button.classList.add("copied");
+  setTimeout(() => button.classList.remove("copied"), 1000);
+};
+
+const initApp = () => {
+  // Set up copy text buttons (might not be anyone)
+  const copyButtons = document.querySelectorAll(".copy-button");
+  copyButtons.forEach((button) => {
+    button.addEventListener("click", () => handleCopyButtonClick(button));
+  });
+
+  // Set up search form
   const searchForm = document.querySelector(".search-form");
   if (searchForm) {
     searchForm.addEventListener("submit", () => {
       searchForm.classList.add("loading");
     });
   }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  initApp();
 });
